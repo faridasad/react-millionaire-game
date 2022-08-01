89aed2cd505107a7776f47130a7dd0d9140f8abc
faridasad <asadlifaridd@gmail.com>
2022-08-01 18:55:46 +0400

    Add data

---
+++ src/data/questions.js
@@ -0,0 +1,68 @@
+export const questionsData = [
+    {
+        id: 1,
+        question: "What is the source of solar energy?",
+        answers: [
+            {
+                text: "Sun",
+                correct: false,
+            },
+            {
+                text: "Water",
+                correct: true,
+            },
+            {
+                text: "Wind",
+                correct: false,
+            },
+            {
+                text: "Wood",
+                correct: false,
+            },
+        ]
+    },
+    {
+        id: 2,
+        question: "Which company was the first to complete a delivery via drone?",
+        answers: [
+            {
+                text: "eBay",
+                correct: false,
+            },
+            {
+                text: "Oba",
+                correct: false,
+            },
+            {
+                text: "Amazon",
+                correct: true,
+            },
+            {
+                text: "Alibaba",
+                correct: false,
+            },
+        ]
+    },
+    {
+        id: 3,
+        question: "In techology, November 30 is known as what day?",
+        answers: [
+            {
+                text: "Techology awareness day",
+                correct: true,
+            },
+            {
+                text: "No Facebook day",
+                correct: false,
+            },
+            {
+                text: "Day of the inventors",
+                correct: false,
+            },
+            {
+                text: "Computer security day",
+                correct: false,
+            },
+        ]
+    },
+]
